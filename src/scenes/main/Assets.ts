3766477192ee95c03c07d09585a035f75b4066f7
Bleak Grey <bleakgrey@e.email>
2023-07-04 21:38:21 +0400

    Add new assets and fonts

--- src/scenes/main/Assets.ts
+++ src/scenes/main/Assets.ts
@@ -1,29 +1,39 @@
-import LIGHT_FONT from '@/assets/fonts/lightFont.fnt?url'
-import LIGHT_FONT_PAGE from '@/assets/fonts/lightFont.png'
+import PRIDI from '@/assets/fonts/pridi.fnt?url'
+import PRIDI_PAGE from '@/assets/fonts/pridi.png'
 
-import PLAYER_SPRITE from '@/assets/images/playerGreen.png?url'
+import PLAYER_SPRITE from '@/assets/images/playerYellow.png?url'
 import ENEMY_SPRITE from '@/assets/images/playerPink.png?url'
 import SHADOW from '@/assets/images/shadow.png?url'
+import SHADOW_GRADIENT from '@/assets/images/shadow_gradient.png?url'
 
 import GRASS from '@/assets/images/grass_top.png?url'
-import TILE from '@/assets/images/dirt.png?url'
 import OBSTACLE from '@/assets/images/fence_wood.png?url'
 import VIGNETTE from '@/assets/images/vignette.png?url'
 
 import BOMB from '@/assets/images/bomb.png?url'
+import WEIGHT from '@/assets/images/weight.png?url'
+import CARROT from '@/assets/images/carrot.png?url'
+
+import SHOOT_ICON from '@/assets/images/icons/shoot.png?url'
+import ROTATE_ICON from '@/assets/images/icons/rotate.png?url'
 
 export default {
-    LIGHT_FONT,
-    LIGHT_FONT_PAGE,
+    PRIDI,
+    PRIDI_PAGE,
 
     PLAYER_SPRITE,
     ENEMY_SPRITE,
     SHADOW,
+    SHADOW_GRADIENT,
 
     GRASS,
-    TILE,
     OBSTACLE,
     VIGNETTE,
 
     BOMB,
+    WEIGHT,
+    CARROT,
+
+    SHOOT_ICON,
+    ROTATE_ICON,
 }
