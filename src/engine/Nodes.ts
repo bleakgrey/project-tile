f7a6c864b6a4b6999f29cf9a622a738f4c1d6c57
Bleak Grey <bleakgrey@e.email>
2023-07-04 21:38:48 +0400

    New TSX tags

--- src/engine/Nodes.ts
+++ src/engine/Nodes.ts
@@ -1,4 +1,5 @@
 import { Container2d, Sprite2d, Text2d } from "pixi-projection";
+import { BitmapText, SimpleRope, Graphics } from "pixi.js";
 import { getTexture, NodeConstructor } from "./Helpers";
 
 export const Sprite: NodeConstructor = (props, children, refs) => {
@@ -15,13 +16,20 @@ export const Sprite: NodeConstructor = (props, children, refs) => {
 
 export const Container: NodeConstructor = (props, children, refs) => {
     const node = new Container2d()
-
     if (props.affine) node.proj.affine = props.affine
-
     return node
 }
 
 export const Label: NodeConstructor = (props, children, refs) => {
-    const node = new Text2d(props.text || '', props.style)
-    return node
+    const initialStyle = props.style || {}
+    const style = Object.assign({ fontName: 'Pridi-Regular', fontSize: 42 }, initialStyle)
+    return new BitmapText(props.text || '', style)
+}
+
+export const Rope: NodeConstructor = (props, children, refs) => {
+    return new SimpleRope(props.texture, props.points)
+}
+
+export const Drawable: NodeConstructor = (props, children, refs) => {
+    return new Graphics()
 }
